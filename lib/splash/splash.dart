ae01694b2c596e9c2deca3ed692f381b8fcf6136
Shahryar Ali <>
2024-03-12 13:02:03 +0500

    - splash implementation

--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -1,45 +1,59 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_playground_project/MyHomePage.dart';
 import 'package:flutter_playground_project/main.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashWidget extends StatefulWidget {
   const SplashWidget({Key? key}) : super(key: key);
 
   @override
-  State<StatefulWidget> createState() => SplashState()
-
+  State<StatefulWidget> createState() => SplashState();
 }
 
-class SplashState extends State<SplashWidget> {
+class SplashState extends State<SplashWidget> with TickerProviderStateMixin {
+  AnimationController? controller;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body:
-        Center(
+        backgroundColor: Theme
+            .of(context)
+            .colorScheme
+            .primary,
+        body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Expanded(
-                child: Container(
-                    color: Colors.white,
-                ),
-              ),
-              Image(image: )
+              SvgPicture.asset(
+                'assets/images/avatar_man.svg', width: 100, height: 100,),
+              const SizedBox(height: 20),
+              const CircularProgressIndicator(color: Colors.white)
             ],
           ),
-        )
-    );
+        ));
   }
 
   @override
   void initState() {
     super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
     navigateToHome();
   }
 
+
+  @override
+  void dispose() {
+    SystemChrome.setEnabledSystemUIMode(
+        SystemUiMode.manual, overlays: SystemUiOverlay.values);
+    super.dispose();
+  }
+
   void navigateToHome() async {
-    Future.delayed(const Duration(milliseconds: 2000), (() {
+    Future.delayed(const Duration(milliseconds: 5000), (() {
       Navigator.pushReplacement(context, MaterialPageRoute(builder: ((context) {
-        return const MyApp();
+        return const MyHomePage(title: "Flutter Demo");
       })));
     }));
   }
